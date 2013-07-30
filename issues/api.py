f54ac9788225a4bf5ed177ba99ca6dedec49b259
Martijn Luinstra <martijnluinstra@gmail.com>
2013-07-30 19:03:30 +0200

    Add comments to API

--- issues/api.py
+++ issues/api.py
@@ -1,19 +1,44 @@
-from flask import jsonify
+from flask import make_response, request
 from flask.ext.login import current_user, login_required
 from issues import app, db
-from models import Issue
+from models import Issue, Comment
+import json
 
 def is_admin():
     if current_user is None or current_user.is_anonymous() or not current_user.admin:
         return False
     return True
 
-@app.route('/api/issues/<int:issue_id>', methods = ['GET'])
-def issues(issue_id):
-    issue = Issue.query.filter_by(id=issue_id, public= not is_admin()).first_or_404()
-    return issue
 
-@app.route('/api/issues/all', methods = ['GET'])
-def issues_all():
-    issues = Issue.query.filter_by(public= not is_admin()).all()
-    return issues
+def jsonify(data):
+    response = make_response(json.dumps(data, indent=2, ensure_ascii=False).encode('utf-8'))
+    response.content_type = 'application/json'
+    return response
+
+
+@app.route('/api/issues/<int:issue_id>', methods=['GET'])
+def view_issue(issue_id):
+    issue = Issue.query.filter_by(id=issue_id, public=not is_admin()).first_or_404()
+    return jsonify(issue.to_dict(compact=False))
+
+
+@app.route('/api/issues/all', methods=['GET'])
+def list_all_issues():
+    issues = Issue.query.filter_by(public=not is_admin()).all()
+    return jsonify([issue.to_dict() for issue in issues])
+
+
+@app.route('/api/issues/<int:issue_id>/comments', methods=['GET'])
+def list_comments(issue_id):
+    issue = Issue.query.filter_by(id=issue_id, public=not is_admin()).first_or_404()
+    comments = issue.comments.all()
+    return jsonify([comment.to_dict() for comment in comments])
+
+
+@app.route('/api/issues/<int:issue_id>/comments', methods=['POST'])
+@login_required
+def add_comment(issue_id):
+    comment = Comment(issue_id, current_user.id, request.get_json()['text'])
+    db.session.add(comment)
+    db.session.commit()
+    return ''
--- issues/models.py
+++ issues/models.py
@@ -2,6 +2,7 @@ from issues import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
+TIME_FORMAT = '%Y:%m:%d %H:%M:%S'
 
 issues_labels = db.Table('issues_labels',
     db.Column('issue', db.Integer, db.ForeignKey('issue.id')),
@@ -44,6 +45,12 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'email': self.email
+        }
+
 
 class Issue(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -56,7 +63,7 @@ class Issue(db.Model):
     comments = db.relationship('Comment', backref='issue',
                                 lazy='dynamic')
     labels = db.relationship('Label', secondary=issues_labels,
-        backref=db.backref('pages', lazy='dynamic'))
+        backref=db.backref('issues', lazy='dynamic'))
 
     def __init__(self, title, description, owner_id, public, deadline=None):
         self.title = title
@@ -66,6 +73,32 @@ class Issue(db.Model):
         self.public = public
         self.deadline = deadline
 
+    def to_dict(self, compact=True):
+        owner = User.query.filter_by(id=self.owner_id).first()
+        if compact:
+            return {
+                'id': self.id,
+                'title': self.title,
+                'description': self.description,
+                'owner': owner.to_dict(),
+                'completed': self.completed,
+                'deadline': self.deadline,
+                'labels': self.labels
+            }
+        comments = self.comments.all()
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'owner': owner.to_dict(),
+            'completed': self.completed,
+            'deadline': self.deadline,
+            'public': self. public,
+            'labels': self.labels,
+            'deadline': self.deadline.isoformat() if self.deadline is not None else None,
+            'comments': list([comment.to_dict() for comment in comments])
+        }
+
 class Comment(db.Model):
     issue_id = db.Column(db.Integer, db.ForeignKey('issue.id'), primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
@@ -73,8 +106,8 @@ class Comment(db.Model):
     time = db.Column(db.DateTime(), primary_key=True)
 
     def __init__(self, issue_id, user_id, text):
-        self.issue_id = issue
-        self.user_id = user
+        self.issue_id = issue_id
+        self.user_id = user_id
         self.text = text
         self.time = datetime.now()
 
@@ -83,7 +116,7 @@ class Comment(db.Model):
             'issue': self.issue_id,
             'user': self.user_id,
             'text': self.text,
-            'time': self.time
+            'time': self.time.isoformat()
         }
 
 class Label(db.Model):
